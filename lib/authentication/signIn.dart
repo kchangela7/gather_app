9b0e0c245f76a289b6f0bcb37ebc4c570d9345b6
ceciliallee <chaelee@email.unc.edu>
2020-10-17 18:44:02 -0400

    sign in

--- lib/authentication/signIn.dart
+++ lib/authentication/signIn.dart
@@ -6,19 +6,84 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+  TextStyle style = TextStyle(fontSize: 20.0);
   @override
   Widget build(BuildContext context) {
+    final email = TextField(
+      child: Text("Email"),
+    );
+    final emailField = TextField(
+      obscureText: false,
+      style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "yourname@school.edu",
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(25.0))),
+    );
+    final passwordField = TextField(
+      obscureText: true,
+      style: style,
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Password",
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(25.0))),
+    );
+
+    final loginButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment(0.8, 0.0),
+              colors: [const Color(0xFFA30C0C), const Color(0xFFE37272)])),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () {},
+        child: Text("Start Matching",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
+
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(height: 50),
-          Text("Sign In"),
-          SizedBox(height: 15),
-          TextField(),
-          SizedBox(height: 15),
-          TextField(),
-        ]
-      )
+      body: Center(
+        child: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(36.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: 50.0,
+                  child: Image.asset(
+                    "assets/images/logo.png",
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                SizedBox(height: 45.0),
+                emailField,
+                SizedBox(height: 25.0),
+                passwordField,
+                SizedBox(
+                  height: 35.0,
+                ),
+                loginButton,
+                SizedBox(
+                  height: 15.0,
+                  width: 300.0,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
